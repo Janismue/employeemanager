9ec86ecd56393fc0d88b613a42eba7d4e72f4266
Janis Müller <janis_mueller@gmx.de>
2021-03-01 17:49:28 +0100

    initial version

---
+++ src/main/java/de/janismueller/employeemanager/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package de.janismueller.employeemanager.service;
+
+import de.janismueller.employeemanager.exception.UserNotFoundException;
+import de.janismueller.employeemanager.model.Employee;
+import de.janismueller.employeemanager.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployee(Long id) {
+        return employeeRepository.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
+    }
+}
